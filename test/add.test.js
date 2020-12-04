b2e0ec763d93e75536b59b2101da83db402217a4
virtuaalilinuksi <markus.hautala@tuni.fi>
2020-12-04 04:20:03 -0800

    add capitalize test 3

--- test/add.test.js
+++ test/add.test.js
@@ -13,6 +13,9 @@ describe('Util library', function() {
     it('should return 0 when adding 0 to 0', function() {
       assert.equal(add(0,0), 0);
     });
+    it('should return 5 when adding 3 to 2', function() {
+      assert.equal(add(3,2), 5);
+    });
   });
 });
 
