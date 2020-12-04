fce655ba41b409825a2e04e4b693fc3193f1886a
virtuaalilinuksi <markus.hautala@tuni.fi>
2020-12-04 04:08:46 -0800

    add capitalize test 2

--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -8,7 +8,7 @@ import add from '../src/capitalize.js';
 describe('Util library', function() {
   describe('capitalize', function() {
     it('should return upper letters to first letter upper and other lower', function() {
-      assert.equal(capitalize("MILK"), "Milk");
+      assert.equal(capitalize('MILK'), 'Milk');
     });
   });
 });
