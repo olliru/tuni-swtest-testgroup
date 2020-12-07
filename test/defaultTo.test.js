16869cc38d8eacd6ee8290a99f833c2d72981e0b
Olli Ruotsalainen <ollijr@gmail.com>
2020-12-06 17:04:15 -0800

    Added tests: defaultTo words

--- test/defaultTo.test.js
+++ test/defaultTo.test.js
@@ -13,6 +13,15 @@ describe('Util library', function() {
     it('should return the first part when there is a first part', function() {
       assert.equal(defaultTo('first', 'second'), 'first');
     });
+    it('should return a numeric value when one is gicen', function() {
+      assert.equal(defaultTo(5, 10), 5);
+    });
+    it('should return a default numeric value when no numeric input is gicen', function() {
+      assert.equal(defaultTo(undefined, 10), 10);
+    });
+    it('should return an empty string when the input is an empty string', function() {
+      assert.equal(defaultTo('', 'default'), '');
+    });
   });
 });
 
--- test/words.test.js
+++ test/words.test.js
@@ -7,8 +7,23 @@ import words from '../src/words.js';
 
 describe('Util library', function() {
   describe('words', function() {
-     it('split words', function() {
-       assert.equal(words('fred'), 'fred');
+     it('splits single word', function() {
+       assert.deepEqual(words('fred'), ['fred']);
+     });
+     it('splits 3 words using default splitter', function() {
+       assert.deepEqual(words('fred, barney, & pebbles'), ['fred', 'barney', 'pebbles']);
+     });
+     it('splits 3 words using custom splitter', function() {
+       assert.deepEqual(words('fred, barney, & pebbles',  /[^, ]+/g), ['fred', 'barney', '&',  'pebbles']);
+     });
+     it('splits words having shitespace', function() {
+       assert.deepEqual(words('fred, bar ney, & pebbles'), ['fred', 'bar', 'ney', 'pebbles']);
+     });
+     it('splits empty string without error', function() {
+       assert.deepEqual(words(''), []);
+     });
+     it('splits whitespace string without error', function() {
+       assert.deepEqual(words('       '), []);
      });
   });
 });
